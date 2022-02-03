1f8bbe6603e928c2429de923ba7c585b3a033a03
Georgii Zhulikov <l8doku@gmail.com>
2022-02-03 12:57:23 +0300

    Update and rephrase tasks

--- code/src/problem2_titanic/main.cpp
+++ code/src/problem2_titanic/main.cpp
@@ -3,16 +3,18 @@
 /// \brief      Main module for Problem 1: Titanic.
 /// \author     Georgii Zhulikov
 /// \author     Sergey Shershakov
-/// \version    0.1.0
-/// \date       01.02.2021
+/// \version    0.1.1
+/// \date       03.02.2022
 ///             This code is for educational purposes of the course "Introduction
 ///             to programming" provided by the Faculty of Computer Science
 ///             at the Higher School of Economics.
 ///
+/// EXAMPLE
 /// 1) Define an alias VecStrings for the std::vector<std::string> datatype
 /// using the typedef keyword.
 ///
-/// 2) Create a function called toCountSurvived that obtains an input stream
+/// EXAMPLE
+/// 2) Create a function called getSurvivorSurnames that obtains an input stream
 /// object (given by reference) istream& (input.csv), reads the whole file
 /// line by line and saves surnames ("Braund; Mr. Owen Harris" will be just
 /// "Braund") of survived people from input.csv (Survived column).
@@ -20,58 +22,64 @@
 ///
 /// Use intermediate functions in task 2 to do the following:
 /// 2.1) Extract data (surname and whether the person survived or not) from one line of input.
-/// 2.2) Extract surname from a string containing full name.
+/// 2.2) (optional) Extract surname from a string containing full name.
 /// 
 ///
+/// PRACTICE
 /// 3) Create a function printVec.
-/// The function prints the content of the given vector out to the standard output.
+/// The function prints the content of the given vector of strings out to the standard output.
 /// It should takes a vector as argument by reference and print the value of the 
 /// elements as well as their enumeration.
-/// 1) Name_1
-/// 2) Name_2
-/// ...
-/// N) Name_n
+///     Example output:
+///     1) Name_1
+///     2) Name_2
+///     ...
+///     N) Name_n
+/// 
+/// 3.1) (Optional homework) Try adding templates to the function, so it can print a vector
+/// of any printable type, not just a vector of strings.
 ///
-///
-///
+/// PRACTICE
 /// 4) Create a function called getFareForClass that takes an input stream object
 /// istream& and an integer number representing class (Pclass, 1 to 3), reads the stream 
 /// until the end and returns the mean fare value of people of the given class.
 /// The function returns a single value of type double -- the mean fair value.
 /// Use at least two intermediate functions in problem 4.
+/// Proposed intermediate functions:
+/// 4.1) Extract needed data from a single line (Pclass and Fare)
+/// 4.2) Compute the mean (average) value of a given vector
 ///
 ///
-/// Additional problems
-///
-/// 5) Create a function called genThreeDigitNumber(const int& randomState).
-/// The function returns a random three digit number as std::string.
-/// Use:
-/// std::mt19937 gen(randomState);
-/// std::uniform_int_distribution<int> distr(0,9);
-/// int rNum = distr(gen); // random number
-///
-/// 6) Create a new vector newVec as VecStrings and fill it with random numbers.
-/// newVec size should be the same as the size of the vector obtained from toCountSurvived
-///
-///
-///   Workshop 8
-///
-/// 6) Reverse the vector containing names of surviving passengers using std::reverse.
+/// EXAMPLE
+/// 5) Reverse the vector containing names of surviving passengers using std::reverse.
 /// Sort this vector using std::sort
 /// Include library <algorithm> to access these functions.
+/// Use iterators.
 ///
-/// 7) Implement a function printVecIter that takes two vector iterators as arguments
+/// EXAMPLE
+/// 6) Implement a function printVecIter that takes two vector iterators as arguments
 /// and prints the elements of the underlying vector. The iterators should represent
 /// the start and the end of the vector.
+/// 
+/// PRACTICE
+/// 7) Change (or implement, if you haven't) the function from task 4.2 to follow the
+/// same logic as the function printVecIter from task 6. Use iterators to compute the
+/// mean value of a vector.
 ///
-/// 8) Use a regular iterator and a reverse iterator (.rbegin()) to print the vector 
+/// PRACTICE
+/// 8) Use a regular iterator and a reverse iterator (.rbegin(), .rend()) to print the vector 
 /// containing survivor names in a straightforward order and in a reverse order
 /// with the function printVecIter.
 /// 
-/// 9) Using the sorted list of surnames find the first and last surname that starts with
-/// the letter "H".
-/// Create a new vector and use functions std::find and std::copy to copy all surnames
-/// starting with "H" into it.
+/// EXAMPLE
+/// 9) Using the sorted list of surnames find the person with surname "Mellors". Use the
+/// function "std::find". The function will return an iterator.
+/// Print the value of this iterator and the next two values.
+/// 
+/// PRACTICE
+/// 10) Find the person with surname "Young" following the example from task 9.
+/// Create a new vector and use functions std::find and std::copy to copy all surnames between
+/// "Mellors" and "Young" into it.
 ///
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -87,7 +95,8 @@
 int main ()
 {
     const std::string INP_FILE_NAME = "../../data/problem2_titanic/titanic.csv";
-    std::ifstream input_file;
-    input_file.open(INP_FILE_NAME);
-
+    std::ifstream inputFile;
+    inputFile.open(INP_FILE_NAME);
+    VecString surnames = getSurvivorSurnames(inputFile);
+    inputFile.close();
 }
